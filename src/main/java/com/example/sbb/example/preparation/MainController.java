4ca1ab6e61e334248aa9dd974fbe0c2f3405f271
pintordev <pintor.dev@gmail.com>
2023-05-09 11:46:29 +0900

    Jump2SB preparation til 2.03

---
+++ src/main/java/com/example/sbb/example/preparation/MainController.java
@@ -0,0 +1,15 @@
+package com.example.sbb.example.preparation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
+}
