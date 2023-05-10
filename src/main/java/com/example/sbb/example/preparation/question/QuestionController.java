d4190bafb76a7119b9dbae4c5492bba55db7e283
pintordev <pintor.dev@gmail.com>
2023-05-10 23:05:25 +0900

    Jump2SB preparation til 2.10

--- src/main/java/com/example/sbb/example/preparation/question/QuestionController.java
+++ src/main/java/com/example/sbb/example/preparation/question/QuestionController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,5 +23,13 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    // 질문 클릭 시 질문 상세 출력 되도록
+    @GetMapping(value = "/detail/{id}") // GetMapping에서 변수로 주소를 설정하는 경우
+    public String detail(Model model, @PathVariable("id") Integer id) { // @PathVariable() 로 매개변수를 받아야 함
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/example/sbb/example/preparation/question/QuestionService.java
+++ src/main/java/com/example/sbb/example/preparation/question/QuestionService.java
@@ -1,11 +1,13 @@
 package com.example.sbb.example.preparation.question;
 
+import com.example.sbb.example.preparation.DataNotFoundException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 @RequiredArgsConstructor
@@ -16,5 +18,16 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> oq = this.questionRepository.findById(id);
+        if (oq.isPresent()) {
+            return oq.get();
+        } else {
+            // 해당 id를 갖는 질문 객체가 존재하지 않는 경우
+            // DataNotFoundException 에러 출력
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
