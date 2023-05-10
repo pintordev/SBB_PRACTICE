da6d9d8b77f013017abc3ef2488b9338cb9dd1da
pintordev <pintor.dev@gmail.com>
2023-05-10 22:45:01 +0900

    Jump2SB preparation til 2.07

---
+++ src/main/java/com/example/sbb/example/preparation/question/QuestionController.java
@@ -0,0 +1,25 @@
+package com.example.sbb.example.preparation.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping("/question")
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+    // 질문 목록 템플릿 구현
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
