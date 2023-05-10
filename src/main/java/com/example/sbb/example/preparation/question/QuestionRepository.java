4001bdad6aee6519c103782b1c024de218957410
pintordev <pintor.dev@gmail.com>
2023-05-10 22:29:53 +0900

    Jump2SB preparation til 2.06

--- src/main/java/com/example/sbb/example/preparation/question/QuestionRepository.java
+++ src/main/java/com/example/sbb/example/preparation/question/QuestionRepository.java
@@ -2,6 +2,10 @@ package com.example.sbb.example.preparation.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+    Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String subject);
 }
--- src/test/java/com/example/sbb/example/preparation/ApplicationTests.java
+++ src/test/java/com/example/sbb/example/preparation/ApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.sbb.example.preparation;
 
+import com.example.sbb.example.preparation.answer.Answer;
+import com.example.sbb.example.preparation.answer.AnswerRepository;
 import com.example.sbb.example.preparation.question.Question;
 import com.example.sbb.example.preparation.question.QuestionRepository;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -17,10 +21,23 @@ class ApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
+	@Autowired
+	private AnswerRepository answerRepository;
+
 	@Test
 	void testJpa() {
-		// 질문을 subject 로 조회 => "sbb가 무엇인가요?"
-		// 조회된 질문의 id가 1인지 조회
+		// id == 2 인 question 객체에 답변 저장
+		// 답변 내용 => "네 자동으로 생성됩니다."
+
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		Answer answer = new Answer();
+		answer.setContent("네 자동으로 생성됩니다");
+		answer.setCreateDate(LocalDateTime.now());
+		answer.setQuestion(q);
+		this.answerRepository.save(answer);
 	}
 
 }
