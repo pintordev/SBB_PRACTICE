d8a3e978933a615a4d664201d01991866fc9b4ba
pintor.dev <pintor.dev@gmail.com>
2023-08-03 01:05:11 +0900

    Update QuestionService.java

--- src/main/java/com/pintor/sbb_practice/question/QuestionService.java
+++ src/main/java/com/pintor/sbb_practice/question/QuestionService.java
@@ -69,7 +69,7 @@ public class QuestionService {
     public Page<Question> getListByUser(int page, SiteUser user) {
         // order by createDate desc;
         List<Sort.Order> sorts = new ArrayList<>(); // 정렬 조건을 저장할 수 있는 Sort.Order 객체로 구성된 List 선언
-        sorts.add(Sort.Order.desc("createDate")); // createDate 속성을 내림차순으로 정렬하는 정렬 조건을 List에 저장
+        sorts.add(Sort.Order.asc("createDate")); // createDate 속성을 내림차순으로 정렬하는 정렬 조건을 List에 저장
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 정렬 조건들이 담긴 List를 넘겨주면 해당 조건에 맞게 정렬 후 해당 페이지를 반환
         return this.questionRepository.findAllByAuthor(user, pageable);
     }
