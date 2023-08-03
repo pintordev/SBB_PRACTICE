9f18a0585b69117b69c210552b6d8390e2779a84
pintor.dev <pintor.dev@gmail.com>
2023-08-03 11:17:19 +0900

    answer and comments count

--- src/main/java/com/pintor/sbb_practice/CommonUtil.java
+++ src/main/java/com/pintor/sbb_practice/CommonUtil.java
@@ -1,5 +1,8 @@
 package com.pintor.sbb_practice;
 
+import com.pintor.sbb_practice.question.Question;
+import com.pintor.sbb_practice.question.QuestionService;
+import lombok.RequiredArgsConstructor;
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -9,8 +12,12 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@RequiredArgsConstructor
 @Component
 public class CommonUtil {
+
+    private QuestionService questionService;
+
     public String markdown(String markdown) {
         Parser parser = Parser.builder().build();
         Node document = parser.parse(markdown);
@@ -25,5 +32,15 @@ public class CommonUtil {
         if (diff.getSeconds() < 60 * 60 * 24) return diff.toHours() + "시간 전";
         if (diff.getSeconds() < 60 * 60 * 24 * 7) return diff.toDays() + "일 전";
         return oldDate.format(DateTimeFormatter.ofPattern("yy.MM.dd"));
+    }
+
+    public Integer getAnswerCommentCount(Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        Integer count = 0;
+        count += question.getAnswerList().size();
+        for (int i = 0; i < question.getAnswerList().size(); i++) {
+            count += question.getAnswerList().get(i).getCommentList().size();
+        }
+        return count;
     }
 }
